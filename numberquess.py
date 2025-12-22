8b6ab83551ef13210c628a34d921ae8f9d46afc9
karthikpai <thekarthikpai@gmail.com>
2025-12-22 21:34:05 +0530

    numberquess

---
+++ numberquess.py
@@ -0,0 +1,8 @@
+n=7
+num=1
+while(n!=num):
+    num=int(input("Enter a NUmber:"))
+    if n!=num:
+      print("wrong")
+else:
+    print("You guessted it correct:")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+n=7
+num=1
+while(n!=num):
+    num=int(input("Enter a NUmber:"))
+    print("wrong")
+else:
+    print("You guessted it correct:")
