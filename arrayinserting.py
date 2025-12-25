af5902e42e512fce1ef538a6f620fcf35fb99ac6
karthikpai <thekarthikpai@gmail.com>
2025-12-25 19:38:31 +0530

    scope  of variable

--- arrayinserting.py
+++ arrayinserting.py
@@ -1,4 +1,12 @@
-a=[1,2,3,4,5]
-ele=9
-newarr=a[0:1]+[ele]+a[1:]
-print(newarr)
+def outer():
+    z = 5   # outer variable
+
+    def inner():
+        nonlocal z
+        z = 10  # modifies outer variable
+        print("Inside inner, z =", z)
+
+    inner()
+    print("Inside outer after inner, z =", z)
+
+outer()
