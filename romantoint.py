ef10a0cf37ca089aa826730bd1433822b5f4be42
karthikpai <thekarthikpai@gmail.com>
2026-01-12 22:27:08 +0530

    Correction

---
+++ romantoint.py
@@ -0,0 +1,18 @@
+def romanToInt(s):
+    roman = {
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100,
+        'D': 500, 'M': 1000
+    }
+
+    total = 0
+    print(len(s))
+    for i in range(len(s)):
+        print(i)
+        if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+            total -= roman[s[i]]
+        else:
+            total += roman[s[i]]
+    return total
+
+print(romanToInt("MCMXCIV"))  # Output: 1994
