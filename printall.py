15e82e243154130a836110552c05bcecffa44ed6
karthikpai <thekarthikpai@gmail.com>
2026-01-16 18:18:20 +0530

    printing all elements

---
+++ printall.py
@@ -0,0 +1,10 @@
+#to print all arithmetic 
+a=int(input("Enter a Number1:"))
+b=int(input("Enter a Number2:"))
+add=a+b
+sub=a-b,
+mul=a*b,
+duv=a/b
+print(add,sub,mul,duv,sep='|')
+print(type(add),type(sub),type(mul)),type(duv)
+
