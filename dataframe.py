23977adf8e5ea14635dc7783ace93b4c13a9a2e9
karthikpai <thekarthikpai@gmail.com>
2026-01-21 09:38:02 +0530

    pandas

---
+++ dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+df=pd.DataFrame(
+    { 
+        'name':['alice','bob','charlie','david'],
+        'age':[25,19,35,40],
+         'city':['newtoe','paris','london','tokyo']
+    }
+)
+
+df.set_index('name',inplace=True)
+print(df)
