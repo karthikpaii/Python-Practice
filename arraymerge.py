472fa8105b68ff560c4cea5809e0f55f237272e4
karthikpai <thekarthikpai@gmail.com>
2026-01-24 22:22:57 +0530

    eDIT

---
+++ arraymerge.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        num1=[1,2,3]
+        num2=[3,4]
+    findMedianSortedArrays(num1,num2)
+
+
+
+        
+
+        
