2ede814fc005e9e88b76875b3655575ca6d1e673
karthikpai <thekarthikpai@gmail.com>
2026-02-10 22:17:16 +0530

    numoy array operations

---
+++ exnumpyprog.py
@@ -0,0 +1,43 @@
+import numpy as np
+list1=[1,2,3]
+x=np.array(list1)
+print(x)
+
+y=np.array([[3,6,4],[8,9,0]])
+print(y)
+
+n=np.array([1,2,3,4,5,6])
+n.shape=(3,2)
+print(n)
+
+#
+m=np.array([2,3,4,5,6,7])
+print(np.reshape(m,(3,2)))
+
+#tranpose
+y=np.array([[3,6,4],[8,9,0]])
+print(np.transpose(y))
+
+#flattening
+print(y.flatten())
+
+arr1=np.array([1,2,3])
+arr2=np.array([1,2,3])
+arr3=np.array([1,2,3])
+print(np.concatenate((arr1,arr2,arr3)))
+
+#concatenation
+ar1=np.array([[1,2,3],[3,2,4]])
+ar2=np.array([[1,2,3],[2,5,6]])
+ar3=np.array([[1,2,3],[1,5,6]])
+print(np.concatenate((ar1,ar2,ar3),axis=1))
+
+#zeros and ones
+print(np.zeros([2,3]))
+print(np.ones([2,3]))
+
+#identity marix
+print(np.identity(5))
+
+#eye
+print(np.eye(5,4,k=-1))
