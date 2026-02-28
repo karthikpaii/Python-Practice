c7eb94c3603de793859221f7ed90287e93f26a49
karthikpai <thekarthikpai@gmail.com>
2026-02-28 17:30:10 +0530

    2nd Legend Method

--- yvalueplot.py
+++ yvalueplot.py
@@ -4,7 +4,7 @@ import numpy as np
 a=[1,2,3,4]
 plt.xlabel("X label")
 plt.ylabel("Y Label")
-plt.plot(a,label="Hi1")
-plt.plot([10,20,30,40],linewidth=2,alpha=0.5,label="Hi1")
+plt.plot(a)
+plt.plot([10,20,30,40],linewidth=2,alpha=0.5)
 plt.legend(["sq","ss"])
 plt.show()
