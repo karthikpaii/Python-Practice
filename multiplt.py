e40c943883188283151ca8934977e4b08838886f
karthikpai <thekarthikpai@gmail.com>
2026-02-28 17:29:17 +0530

    labeling legend

---
+++ multiplt.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x=np.linspace(0,10,100)
+y=np.sin(x)
+plt
+print(x)
+print(y)
--- yvalueplot.py
+++ yvalueplot.py
@@ -4,6 +4,7 @@ import numpy as np
 a=[1,2,3,4]
 plt.xlabel("X label")
 plt.ylabel("Y Label")
-plt.plot(a,linestyle="dotted",marker="*", markeredgecolor="red",markersize=13)
-plt.legend("His")
+plt.plot(a,label="Hi1")
+plt.plot([10,20,30,40],linewidth=2,alpha=0.5,label="Hi1")
+plt.legend(["sq","ss"])
 plt.show()
