8f9a6cd047811f6c195c14fad636f3d750c882df
karthikpai <thekarthikpai@gmail.com>
2026-03-01 15:39:56 +0530

    Python Function Related PRoblem -UNit 2

---
+++ creatingoddnumber.py
@@ -0,0 +1,6 @@
+a=[]
+for i in range(1,20,2):
+
+    a.append(i)
+
+print(a)
---
+++ formatedstring.py
@@ -0,0 +1,6 @@
+name="DEV"
+age="21"
+
+print("My name is {} and age is {}".format(name,age))
+
+print(f"My Name is:  {name} and age is: {age}")
